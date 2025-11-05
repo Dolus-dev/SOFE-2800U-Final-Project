6602fa5264f2dd0e2d90a6a70f95460cf2f5130d
Faceless-Dolulu <andrei.anastasiu@outlook.com>
2025-11-05 15:14:04 -0500

    Created User Dropdown Menu Component

---
+++ apps/frontend/app/components/Header/SessionDropdown.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { useOutsideClick } from "@/app/lib/handleOutsideClick";
+import {
+	ArrowLeftStartOnRectangleIcon,
+	ChevronDownIcon,
+	ChevronUpIcon,
+	UserCircleIcon,
+} from "@heroicons/react/24/solid";
+import Link from "next/link";
+import { useEffect, useRef, useState } from "react";
+
+// TODO: Change output depending on whether is logged in based on fetch call
+// Above could be handled either in parent page or locally here
+
+export default function SessionDropdown() {
+	const [isOpen, setIsOpen] = useState(false);
+	const dropdownRef = useOutsideClick(() => {
+		setIsOpen(false);
+	});
+
+	return (
+		<div
+			className="flex flex-col relative"
+			ref={dropdownRef}>
+			<button
+				className={`overflow-hidden flex flex-row z-10 items-center hover:cursor-pointer py-2 border-b-2 border-b-lightSecondary`}
+				onClick={() => setIsOpen(!isOpen)}>
+				<UserCircleIcon className="size-8" />
+				<span className="px-2 mr-10">Test User</span>
+				<div className={`transition-normal duration-500 `}>
+					<ChevronDownIcon
+						className={`  absolute  top-3 right-1 duration-500   transform ${
+							isOpen ? "opacity-0 scale-25 rotate-90" : "opacity-100 scale-100"
+						} size-6 `}
+					/>
+					<ChevronUpIcon
+						className={` absolute transform top-3 right-1 duration-500 ${isOpen ? "opacity-100 scale-100 rotate-0" : "opacity-0 scale-25 rotate-90"} size-6 `}
+					/>
+				</div>
+			</button>
+			<div
+				className={`flex flex-col absolute overflow-hidden w-full font-semibold justify-center border-b-2 border-b-accent bg-lightSecondary top-full text-lightText ${isOpen ? "  max-h-[2em]" : " max-h-0 "}  transition-normal duration-500`}>
+				<Link
+					href={"/auth/signout"}
+					className={`flex flex-row justify-center gap-2 hover:text-lightTextMuted  overflow-hidden py-1 transition-normal duration-400`}>
+					<ArrowLeftStartOnRectangleIcon className="size-6" />
+					<span>Logout</span>
+				</Link>
+			</div>
+		</div>
+	);
+}
--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,28 +1,27 @@
+import SessionDropdown from "./components/Header/SessionDropdown";
 
-
-// This is the landing page for the TODO List 
+// This is the landing page for the TODO List
 export default function Home() {
 	return (
-		<div className="flex flex-col h-dvh justify-between text-black">
-			<header className="h-10 bg-lightSecondary">
-					Header
+		<div className="flex flex-col h-dvh justify-between text-black ">
+			<header className=" bg-lightSecondary flex flex-row justify-end border-b-accent border-b-2">
+				<SessionDropdown />
 			</header>
 			<main className="mb-auto h-full bg-lightPrimary">
-			<div className="flex flex-col items-center justify-center">
-				<h1 className="font-bold text-lightText text-5xl sm:text-6xl md:text-8xl lg:text-9xl mt-20 sm:mt-25 md:mt-25 lg:mt-40">
-					Tick-It
-				</h1>
-				<h2 className="font-semibold text-lightText text-3xl sm:text-4xl md:text-6xl lg:text-7xl mt-8">
-					Got it done? Just Tick-it.
-				</h2>
-			</div>
+				<div className="flex flex-col items-center justify-center">
+					<h1 className="font-bold text-lightText text-5xl sm:text-6xl md:text-8xl lg:text-9xl mt-20 sm:mt-25 md:mt-25 lg:mt-40">
+						Tick-It
+					</h1>
+					<h2 className="font-semibold text-lightText text-3xl sm:text-4xl md:text-6xl lg:text-7xl mt-8">
+						Got it done? Just Tick-it.
+					</h2>
+				</div>
 			</main>
 			<footer className="bg-lightSecondary  flex flex-col">
 				<div className="place-self-center">
 					<p> Tick-It &copy; 2025</p>
 				</div>
 			</footer>
-			
 		</div>
 	);
 }
