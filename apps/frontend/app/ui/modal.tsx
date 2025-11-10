5a7b46b6ec73e8eb3337b3a3c217932a460df5b7
Faceless-Dolulu <andrei.anastasiu@outlook.com>
2025-11-10 14:29:49 -0500

    Removed an outdated modal component used for learning/testing

--- apps/frontend/app/ui/modal.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { useRouter } from "next/navigation";
-
-export function Modal({ children }: { children: React.ReactNode }) {
-	const router = useRouter();
-
-	return { children };
-}
